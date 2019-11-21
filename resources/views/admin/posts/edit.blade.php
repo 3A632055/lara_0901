9f4872193e4defa004ec8e5a1804ae7e61ad7f1c
JiaXing <s3a632055@gm.student.ncut.edu.tw>
2019-11-22 01:43:24 +0800

    < 練習4-1> 將錯誤訊息顯示在畫面上

--- resources/views/admin/posts/edit.blade.php
+++ resources/views/admin/posts/edit.blade.php
@@ -17,12 +17,17 @@
     </div>
 </div>
 <!-- /.row -->
+<!--單元練習< 練習3-1> 編輯 create.blade.php 及 edit.blade.php-->
 @if ($errors‐>any())
 <div class="row">
     <div class="col-lg-12">
         <div class="alert alert-danger alert-dismissable">
             <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
             <i class="fa fa-info-circle"></i>  <strong>警告！</strong> 請修正表單錯誤：
+            <!--單元練習< 練習4-1> 將錯誤訊息顯示在畫面上-->
+            @foreach($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
         </div>
     </div>
 </div>
